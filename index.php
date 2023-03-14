9be3e554721e3a6e30d67e9c9dafbf9fa97287cf
Jordan <tiagojordan2009@gmail.com>
2023-03-14 10:49:18 -0300

    arquivo de cabecalho do administrador

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__.'\templates\cabecalho_user.php';
+?>
+
+    <p>
+        <a href="controllers/login.php"></a>
+    </p>
+
+
+<?php
+require_once __DIR__.'\templates\rodape.php';
+?>
