2aadebb3469ba48844c8ba374e1f125edd92e826
Jordan <tiagojordan2009@gmail.com>
2023-03-14 09:42:08 -0300

    arquivo de configuracao acesso banco php

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+/*
+*  Configuração de acesso ao banco de dados
+*  Modelo do Banco "MySql"
+*  
+*  Contatos
+*   tiagojordan@outlook.com
+*   salespintorafael@gmail.com
+*/
+
+//      Tipo do banco
+define('DB_MYSQL',      'mysql'     );
+//      IP do Servidor
+define('IP_SERVE',      '127.0.0.1' );
+//      Nome do Banco
+define('DB_NOME',       'linxbotdb' );
+//      Usuario de acesso
+define('DB_USER',       'root'      );
+//      Senha de acesso
+define('DB_PASS',       ''          );
