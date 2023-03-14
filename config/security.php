f439d2709680c5ec58311e47fd0cedb7b4847785
Jordan <tiagojordan2009@gmail.com>
2023-03-14 09:44:32 -0300

    arquivo de seguranca niveis de acesso

---
+++ config/security.php
@@ -0,0 +1,20 @@
+<?php
+/*
+*  Hash Base64 encode para autenticação de niveis de acessos 
+*  Admin: acesso adminstrador para cadastro mestre
+*  User:  acesso padrao de usuario comun
+*  Conf:  permissao para configuracões 'Cadastro user', 'Cadastro cardapio' etc.
+*  
+*  
+*  Contatos
+*   tiagojordan@outlook.com
+*   salespintorafael@gmail.com
+*/
+
+
+//      HashPassAdm
+define('ACESSO_ADIM', 'SGFzaFBhc3NBZG0=');
+//      HashPassUsr
+define('ACESSO_USER', 'SGFzaFBhc3NVc3I=');
+//      HashPassCnf
+define('ACESSO_CONF', 'SGFzaFBhc3NDbmY=');
