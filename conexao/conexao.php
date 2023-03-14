52c2c89232380a52793df86d13b78f2f2added64
Jordan <tiagojordan2009@gmail.com>
2023-03-14 15:33:42 -0300

    arquivo de conexao com o banco de dados

---
+++ conexao/conexao.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Class para chamada da conexao com o banco de dados
+ * passando os paramentros do arquivo de configuracao
+ * 
+ * 
+ *  Contatos
+ *   tiagojordan@outlook.com
+ *   salespintorafael@gmail.com
+ * 
+ * 
+ */
+require_once __DIR__.'\\config\\config.php';
+
+class Conexao {
+    public static function conectar(){
+        $conexao = new PDO(DB_MYSQL . ":host=" . IP_SERVE . ";dbname=" . DB_NOME, DB_USER, DB_PASS);
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conexao;
+    }
+}
+
