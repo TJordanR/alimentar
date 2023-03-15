69c66f8a3e7d47443a23d04aeae0dba6ad60c987
TJordanR <tiagojordan2009@gmail.com>
2023-03-14 22:33:46 -0300

    atualizacao do arquivo index principal

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php
 /// padrao para usuario comum
-require_once __DIR__.'\templates\cabecalho_user.php';
+require_once 'templates/cabecalho_user.php';
+
+try {
+    $lista = usuario::teste_tab();
+} catch(Exception $retorno){
+    echo $retorno->getMassage();
+}
 ?>
 
     <p>
         <a href="controllers/login.php"></a>
     </p>
 
+    <div class="container_cards">
+        <?php foreach ($lista as $usuario) : ?>
+            <h1><?= $usuario['id_usuario'] ?></h1>
+        <?php endforeach; ?>
+    </div>
+
 
 <?php
-require_once __DIR__.'\templates\rodape.php';
+require_once 'templates/rodape.php';
 ?>
