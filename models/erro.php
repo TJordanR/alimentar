2e0d5217e0aa5f9b6fb2758fe61ba83f78f1737e
TJordanR <tiagojordan2009@gmail.com>
2023-03-15 23:34:48 -0300

    inclusao de uma classe de retorno de erros

---
+++ models/erro.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Erro {
+    public static function erro_class(){
+        $erro='';
+        return $erro;
+        $retorno_erro ='index.php';
+        echo"<h1>Retorno do Erro na pagina index referentea:</h1>";
+        echo"<h3>";
+        echo $retorno_erro;
+        echo "</h3>";
+    }
+}
