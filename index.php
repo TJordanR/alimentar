986f642cd48dd254bab81e8597cf1e1c503ab088
TJordanR <tiagojordan2009@gmail.com>
2023-03-15 23:26:48 -0300

    alteracao do index principal para secundario

--- index.php
+++ index.php
@@ -1,25 +1,28 @@
 <?php
-/// padrao para usuario comum
-require_once 'templates/cabecalho_user.php';
 
-try {
-    $lista = usuario::teste_tab();
-} catch(Exception $retorno){
-    echo $retorno->getMassage();
-}
+    /**
+     * 
+     * Olá, pessoal! Hoje eu gostaria de apresentar a vocês o nosso novo projeto
+     * chamado Alimentar, uma aplicação web feita especialmente para restaurantes. 
+     * Com o Alimentar, você poderá organizar quantitativamente os pedidos dos 
+     * clientes, definir os cardápios de acordo com os dias da semana e ainda somar 
+     * os valores vendidos diariamente.
+     * Com a capacidade de fazer o controle semanal e mensal das vendas de acordo com 
+     * os cardápios disponíveis e a quantidade de cada um deles, o Alimentar é a 
+     * ferramenta ideal para gerenciar o seu restaurante. E o melhor de tudo é que os 
+     * layouts são simples e intuitivos, para que qualquer pessoa possa usá-lo sem 
+     * problemas. O Alimentar também permite que você faça o controle de acesso para 
+     * empresas, tendo a possibilidade de cadastrar os funcionários com suas respectivas 
+     * matrículas. Além disso, ele tem uma tela de acesso inicial, login de acesso para 
+     * administrador, usuário master e usuário comum, tela de pedido e autenticação do 
+     * funcionário ou cliente.
+     * Não importa o tamanho do seu restaurante, o Alimentar é a solução que você precisa 
+     * para melhorar a organização e aumentar as suas vendas. Então, não perca mais tempo e 
+     * venha conhecer essa ferramenta incrível que vai revolucionar a sua forma de gerenciar 
+     * o seu negócio. Obrigado pela atenção!
+     * 
+     */
+    header('location: views/index.php');
 ?>
 
-    <p>
-        <a href="controllers/login.php"></a>
-    </p>
 
-    <div class="container_cards">
-        <?php foreach ($lista as $usuario) : ?>
-            <h1><?= $usuario['id_usuario'] ?></h1>
-        <?php endforeach; ?>
-    </div>
-
-
-<?php
-require_once 'templates/rodape.php';
-?>
