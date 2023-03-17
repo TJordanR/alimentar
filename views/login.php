f3e5bae50200478f2032703799fdcb59cbc0cb6c
TJordanR <tiagojordan2009@gmail.com>
2023-03-16 23:12:06 -0300

    alteracao no arquivo de login

--- views/login.php
+++ views/login.php
@@ -1,31 +1,41 @@
 <?php
 require_once '../templates/cabecalho_user.php';
-if(isset($_SESSION['id_usuario'])){
+
+if (isset($_SESSION['usuario'])) {
     header('location: index.php');
 }
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $user_nome = $_REQUEST['user_nome'];
-    $user_senha = $_REQUEST['user_senha']; 
+    $nome_usuario  = $_REQUEST['u_user'];
+    $senha_usuario = $_REQUEST['u_senha'];
 
     try {
-        $query = "SELECT * FROM usuarios WHERE user_nome = :user_nome LIMIT 1";
+        $query = "select * from usuarios where user_nome = :u_user LIMIT 1";
         $conexao = Conexao::conectar();
         $stmt = $conexao->prepare($query);
-        $stmt->bindValue(":user_nome", $user_nome);
+        $stmt->bindValue(":u_user", $nome_usuario);
         $stmt->execute();
         $registro = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if ($stmt->rowCount() > 0) {
-            if (password_verify($user_senha, $registro['user_senha'])) {
-                $_SESSION['usuario']['user_nome']  = $registro['user_nome'];
-                $_SESSION['usuario']['user_senha'] = $registro['u_suser_senhaenha'];
-                header('location: erro.php');
+
+            // SENHA COM HASHE 
+            //if (password_verify($senha, $registro['senha'])) {
+
+            if ($senha_usuario = $registro['user_senha']) {
+                echo"<h1>";
+                echo"logim aceito";
+                echo"</h1>";
+                header('location: index.php');
             } else {
-                $erroMsg[] = "Usuario ou Senha errado";
+                $erroMsg[] = "Email ou Senha errado";
+                echo $senha_usuario;
+                echo $nome_usuario;
+                echo"logim aceito 1";
             }
         } else {
-            $erroMsg[] = "Usuario ou Senha errado";
+            $erroMsg[] = "Email ou Senha errado";
+            echo"logim aceito 2";
         }
     } catch (Exception $e) {
         echo $e->getMessage();
@@ -36,14 +46,13 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 <form action="" method="POST">
     <br>
-    <label for="user_nome">Nome de acesso</label>
-    <input type="text" name="user_nome" id="user_nome" placeholder="Nome do Usuario">
+        <label for="u_user">Nome de acesso</label>
+        <input type="text" name="u_user" id="u_user" placeholder="Nome do Usuario">
     <br>
-    <label for="user_senha">Senha de acesso</label>
-    <input type="text" name="user_senha" id="user_senha" placeholder="Senha do Usuario">
+        <label for="u_senha">Senha de acesso</label>
+        <input type="text" name="u_senha" id="u_senha" placeholder="Senha do Usuario">
     <br>
     <button type="submit">Acessar</button>
-
 </form>
 
 <?php
